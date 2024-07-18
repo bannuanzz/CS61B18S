0799a28c5a284b15357cfd157a7a67dd46ee83f4
Morri <834301547@qq.com>
2024-07-18 20:15:15 +0800

    proj0-4-fix the negative

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -44,14 +44,14 @@ public class Planet {
     public double calcForceExertedByX(Planet p) {
         double f = calcForceExertedBy(p);
         double distance = calcDistance(p);
-        double disX = this.xxPos - p.xxPos;
+        double disX = p.xxPos - this.xxPos;
         return disX * f / distance;
     }
 
     public double calcForceExertedByY(Planet p) {
         double f = calcForceExertedBy(p);
         double distance = calcDistance(p);
-        double disY = this.yyPos - p.yyPos;
+        double disY = p.yyPos - this.yyPos;
         return disY * f / distance;
     }
 
