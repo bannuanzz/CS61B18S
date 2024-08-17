8898f2dffc6d492bc60f525441fa719ef318bb5c
Morri <834301547@qq.com>
2024-08-17 22:49:04 +0800

    fix the mistake that using the father's method and return son's type in Palindrome.java

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,7 +1,7 @@
 public class Palindrome {
 
 
-    public LinkedListDeque<Character> wordToDeque(String word) {
+    public Deque<Character> wordToDeque(String word) {
         LinkedListDeque<Character> d = new LinkedListDeque<>();
         for (int i = 0; i < word.length(); i++) {
             d.addLast(word.charAt(i));
