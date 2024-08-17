d60e6a3644de10899d2b4afa639afa7f2e36c43f
Morri <834301547@qq.com>
2024-08-17 22:37:49 +0800

    add the isPalindrome overload

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,4 +1,6 @@
 public class Palindrome {
+    public boolean isPalindrome
+
     public LinkedListDeque<Character> wordToDeque(String word) {
         LinkedListDeque<Character> d = new LinkedListDeque<>();
         for (int i = 0; i < word.length(); i++) {
@@ -6,6 +8,22 @@ public class Palindrome {
         }
         return d;
     }
+
+    public boolean isPalindrome(String word) {
+        if (word == null || word.length() == 1) {
+            return true;
+        }
+        int i = 0, j = word.length() - 1;
+        while (i <= j) {
+            if (word.charAt(i) != word.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
 
     public boolean isPalindrome(String word, CharacterComparator cc) {
         if (word == null || word.length() == 1) {
